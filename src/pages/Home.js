941f43f7e6a3d56d91769cd43ef3db1dc4dc42dc
annaVih <annavi@MML801.local>
2019-10-06 18:09:02 +0100

    Configure routing and render banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
-    <div>
-      Hello from home page
-    </div>
+    <Hero hero="defaultHero">
+      <Banner title="Luxurious rooms" subtitle="deluxe">
+        <Link to="/rooms" className="btn-primary">our rooms</Link>
+      </Banner>
+    </Hero>
   )
+}
+
+Hero.defaultProps = {
+  hero: "defaultHero"
 }
 
 export default Home
